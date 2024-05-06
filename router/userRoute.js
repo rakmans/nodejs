1ceb0cb02be57614ccc830e06603b2140d6bdbd7
mahdiRakman <168580151+mahdiRakman@users.noreply.github.com>
2024-05-06 22:33:15 +0330

    Update userRoute.js

--- router/userRoute.js
+++ router/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { login } from "../controllers/userController.js";
+import { dashboard, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/login/user", login);
-
+router.get("/dashboard", dashboard);
 export default router;
